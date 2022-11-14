004c41d3346ea3df71a212b0442d459fb50bec1a
Axel Berardino <axel@zen.ly>
2022-11-14 01:46:53 +0100

    Introduce clap for the command handling

--- projects/piretoutpire/src/bin/main.rs
+++ projects/piretoutpire/src/bin/main.rs
@@ -1,53 +1,44 @@
-use errors::{bail, AnyError, AnyResult};
+use clap::{ArgEnum, Parser};
+use errors::AnyResult;
 use piretoutpire::manager::manager::Manager;
 use std::path::Path;
 
-enum ConnectionType {
+#[derive(Parser)]
+#[clap(name = "pire2pire")]
+#[clap(about = "Pire2Pire CLI", long_about = None)]
+
+struct Cli {
+    /// Server host:port
+    #[clap(default_value_t = String::from("localhost:4000"))]
+    #[clap(long, value_name = "host:port")]
+    server: String,
+
+    /// What mode to run the program in
+    #[clap(arg_enum)]
+    #[clap(long = "type", value_name = "type")]
+    query_type: QueryType,
+}
+
+// Mode ------------------------------------------------------------------------
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum, Debug)]
+enum QueryType {
     Seeder,
     Leecher,
 }
 
-impl TryFrom<&str> for ConnectionType {
-    type Error = AnyError;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        Ok(match value {
-            "seeder" => Self::Seeder,
-            "leecher" => Self::Leecher,
-            _ => bail!("unknown type {}", value),
-        })
-    }
-}
-
 #[tokio::main]
 async fn main() -> AnyResult<()> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() <= 1 {
-        panic!("missing action (client or server)");
-    }
-    if args.len() <= 2 {
-        panic!("missing host:port");
-    }
-    // if args.len() <= 3 {
-    //     panic!("missing working folder");
-    // }
-    println!("{:?}", args);
+    let args = Cli::parse();
 
-    let connection_type: ConnectionType = args[1].as_str().try_into()?;
-    let _addr = args[2].clone();
-    let _file = if !args[3].is_empty() {
-        args[3].clone()
-    } else {
-        "/tmp/toto.txt".to_owned()
-    };
-    match connection_type {
-        ConnectionType::Seeder => {
+    match &args.query_type {
+        QueryType::Seeder => {
             let server_own_addr = "127.0.0.1:4000".parse()?;
             let mut manager = Manager::new(0, server_own_addr, "/tmp/seeder".to_owned());
             manager.load_file("/tmp/seeder/toto.txt").await?;
             manager.start_server().await?;
         }
-        ConnectionType::Leecher => {
+        QueryType::Leecher => {
             let own_addr = "127.0.0.1:4001".parse()?;
             let peer_addr = "127.0.0.1:4000".parse()?;
             let mut manager = Manager::new(1, own_addr, "/tmp/leecher".to_owned());
